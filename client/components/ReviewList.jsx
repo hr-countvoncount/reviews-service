7e7c215044836ad712c11747ed8aa7a62b8be386
Michael Roeslein <michaeljroeslein@gmail.com>
2019-04-27 14:53:59 -0400

    Troubleshooting environmental issues

--- client/components/ReviewList.jsx
+++ client/components/ReviewList.jsx
@@ -33,11 +33,11 @@ class ReviewList extends React.Component {
   }
 
   getReviews(id) {
+    console.log("MAyBE THIS IS A PROBLEM ", process.env.DB_DBNANE);
+    console.log("I HAVE ACCESS RIGHT ? ", process.env.AWS_IP);
     return axios({
       // url: `http://localhost:3002/room${id}`,
-      url: `http://ec2-${
-        process.env.AWS_IP
-      }.us-east-2.compute.amazonaws.com/room${id}`,
+      url: `http://${process.env.AWS_IP}:3002/room${id}`,
       method: "get",
       params: { limit: 7, offset: this.state.offset }
     })
@@ -68,11 +68,7 @@ class ReviewList extends React.Component {
   getSearchResults(id, word) {
     axios
       // .get(`http://localhost:3002${id}/search/${word}`)
-      .get(
-        `http://ec2-${
-          process.env.AWS_IP
-        }.us-east-2.compute.amazonaws.com${id}/search/${word}`
-      )
+      .get(`http://${process.env.AWS_IP}:3002${id}/search/${word}`)
       .then(({ data }) => {
         this.setState({
           searchedReviews: data,
