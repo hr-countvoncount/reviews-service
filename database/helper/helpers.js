7c38d39df31c214027342f49d4855f42ca3b2f68
Michael Roeslein <michaeljroeslein@gmail.com>
2019-04-30 11:39:40 -0400

    Updated pool for Sequelize Instance 3

--- database/helper/helpers.js
+++ database/helper/helpers.js
@@ -11,7 +11,13 @@ const sequelize = new Sequelize(
     host: `${process.env.DB_HOST}`,
     dialect: `${process.env.DB_DIALECT}`,
     port: `${process.env.DB_PORT}`,
-    logging: false
+    logging: false,
+    pool: {
+      max: 30,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 );
 
