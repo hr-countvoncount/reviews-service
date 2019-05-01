b5d42f3e752b17d14eb0dc96b6220dc73bbe2f2f
Michael Roeslein <michaeljroeslein@gmail.com>
2019-05-01 16:37:41 -0400

    Updated IP for Instance 3

--- database/helper/pg-helpers.js
+++ database/helper/pg-helpers.js
@@ -1,20 +1,20 @@
 const Pool = require("pg-pool");
 
-// const pool = new Pool({
-//   user: `${process.env.DB_USERNAME}`,
-//   host: `${process.env.DB_HOST}`,
-//   database: `${process.env.DB_DBNAME}`,
-//   password: `${process.env.DB_PASSWORD}`,
-//   port: `${process.env.DB_PORT}`,
-//   max: 20,
-//   idleTimeoutMillis: 30000,
-//   connectionTimeoutMillis: 2000
-// });
-
 const pool = new Pool({
-  database: "reviews",
-  host: "localhost"
+  user: `${process.env.DB_USERNAME}`,
+  host: `${process.env.DB_HOST}`,
+  database: `${process.env.DB_DBNAME}`,
+  password: `${process.env.DB_PASSWORD}`,
+  port: `${process.env.DB_PORT}`,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000
 });
+
+// const pool = new Pool({
+//   database: "reviews",
+//   host: "localhost"
+// });
 
 const getReviewsFromDatabase = async (id, callback) => {
   try {
