c4c79c32a703bb742f87a764d2f0883587d07aeb
Michael Roeslein <michaeljroeslein@gmail.com>
2019-05-02 14:33:06 -0400

    Refactored SQL query to use Join

--- database/helper/pg-helpers.js
+++ database/helper/pg-helpers.js
@@ -4,14 +4,27 @@ const getReviewsFromDatabase = async (id, callback) => {
   try {
     const res = await pool.query(
       `SELECT users.name, users.avatar, reviews.date, reviews.text, reviews.rating, reviews.has_response, reviews.owner_response
-      FROM users, reviews
-      WHERE users.id = reviews.user_id AND reviews.apartment_id = ${id};`
+      FROM users JOIN reviews
+      ON users.id = reviews.user_id AND reviews.apartment_id = ${id};`
     );
     callback(null, res.rows);
   } catch (err) {
     console.log(err);
   }
 };
+
+// const getReviewsFromDatabase = async (id, callback) => {
+//   try {
+//     const res = await pool.query(
+//       `SELECT users.name, users.avatar, reviews.date, reviews.text, reviews.rating, reviews.has_response, reviews.owner_response
+//       FROM users, reviews
+//       WHERE users.id = reviews.user_id AND reviews.apartment_id = ${id};`
+//     );
+//     callback(null, res.rows);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
 
 const getSearchResultsFromDatabase = async (id, word, callback) => {
   try {
